17cc09248a5b0e0b5be526ac0ad7326ff07872a7
danunicomp <dshaffer@pckeyboard.com>
2016-06-02 11:13:09 -0400

    Initial Upload
    Works, but none of the buttons or position works

---
+++ Pointing Stick C Sharp/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Pointing_Stick_C_Sharp
+{
+    partial class frmPSTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDriftResult = new System.Windows.Forms.Button();
+            this.btnRightClick = new System.Windows.Forms.Button();
+            this.btnLeftClick = new System.Windows.Forms.Button();
+            this.btnRememberPos = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.lblMouseCoord = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtXpos = new System.Windows.Forms.TextBox();
+            this.txtYpos = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDriftResult
+            // 
+            this.btnDriftResult.Location = new System.Drawing.Point(13, 356);
+            this.btnDriftResult.Name = "btnDriftResult";
+            this.btnDriftResult.Size = new System.Drawing.Size(171, 45);
+            this.btnDriftResult.TabIndex = 18;
+            this.btnDriftResult.UseVisualStyleBackColor = true;
+            // 
+            // btnRightClick
+            // 
+            this.btnRightClick.Location = new System.Drawing.Point(109, 309);
+            this.btnRightClick.Name = "btnRightClick";
+            this.btnRightClick.Size = new System.Drawing.Size(75, 40);
+            this.btnRightClick.TabIndex = 17;
+            this.btnRightClick.Text = "RIGHT CLICK";
+            // 
+            // btnLeftClick
+            // 
+            this.btnLeftClick.Location = new System.Drawing.Point(13, 309);
+            this.btnLeftClick.Name = "btnLeftClick";
+            this.btnLeftClick.Size = new System.Drawing.Size(75, 40);
+            this.btnLeftClick.TabIndex = 16;
+            this.btnLeftClick.Text = "LEFT CLICK";
+            // 
+            // btnRememberPos
+            // 
+            this.btnRememberPos.Location = new System.Drawing.Point(425, 343);
+            this.btnRememberPos.Name = "btnRememberPos";
+            this.btnRememberPos.Size = new System.Drawing.Size(75, 23);
+            this.btnRememberPos.TabIndex = 21;
+            this.btnRememberPos.Text = "F4 - Lock";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(425, 378);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "F10 - Exit";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(425, 309);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 19;
+            this.btnRestart.Text = "F1 - Restart";
+            // 
+            // Panel1
+            // 
+            this.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Panel1.Controls.Add(this.lblMouseCoord);
+            this.Panel1.Controls.Add(this.Label2);
+            this.Panel1.Controls.Add(this.Label1);
+            this.Panel1.Controls.Add(this.txtXpos);
+            this.Panel1.Controls.Add(this.txtYpos);
+            this.Panel1.Location = new System.Drawing.Point(217, 309);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(144, 64);
+            this.Panel1.TabIndex = 22;
+            // 
+            // lblMouseCoord
+            // 
+            this.lblMouseCoord.AutoSize = true;
+            this.lblMouseCoord.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMouseCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMouseCoord.Location = new System.Drawing.Point(8, 8);
+            this.lblMouseCoord.Name = "lblMouseCoord";
+            this.lblMouseCoord.Size = new System.Drawing.Size(127, 18);
+            this.lblMouseCoord.TabIndex = 7;
+            this.lblMouseCoord.Text = "Mouse Coordinates";
+            this.lblMouseCoord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(16, 32);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(19, 16);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "X:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(80, 32);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(20, 16);
+            this.Label1.TabIndex = 10;
+            this.Label1.Text = "Y:";
+            // 
+            // txtXpos
+            // 
+            this.txtXpos.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtXpos.Location = new System.Drawing.Point(32, 32);
+            this.txtXpos.Name = "txtXpos";
+            this.txtXpos.Size = new System.Drawing.Size(32, 20);
+            this.txtXpos.TabIndex = 8;
+            this.txtXpos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtYpos
+            // 
+            this.txtYpos.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtYpos.Location = new System.Drawing.Point(96, 32);
+            this.txtYpos.Name = "txtYpos";
+            this.txtYpos.Size = new System.Drawing.Size(32, 20);
+            this.txtYpos.TabIndex = 9;
+            this.txtYpos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(488, 280);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmPSTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 416);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Panel1);
+            this.Controls.Add(this.btnRememberPos);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnDriftResult);
+            this.Controls.Add(this.btnRightClick);
+            this.Controls.Add(this.btnLeftClick);
+            this.Name = "frmPSTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C Sharp";
+            this.Load += new System.EventHandler(this.frmPSTest_Load);
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button btnDriftResult;
+        internal System.Windows.Forms.Button btnRightClick;
+        internal System.Windows.Forms.Button btnLeftClick;
+        internal System.Windows.Forms.Button btnRememberPos;
+        internal System.Windows.Forms.Button btnExit;
+        internal System.Windows.Forms.Button btnRestart;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.Label lblMouseCoord;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtXpos;
+        internal System.Windows.Forms.TextBox txtYpos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Pointing Stick C Sharp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Pointing_Stick_C_Sharp
+{
+
+   
+
+    public partial class frmPSTest : Form
+    {
+        Point lastPoint = Point.Empty;//Point.Empty represents null for a Point object
+        private Graphics paper;
+        private Pen pen;
+    
+
+        public frmPSTest()
+        {
+            InitializeComponent();
+            paper = this.CreateGraphics();
+            pen = new Pen(Color.Black);
+            pen.Width = 1;
+
+            pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(pictureBox1_MouseMove);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(panel2_MouseMove);
+        }
+
+        private void frmPSTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            //  Console.WriteLine("Move:");
+
+
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel2_MouseEnter(object sender, System.EventArgs e)
+        {
+            // Update the mouse event label to indicate the MouseEnter event occurred.
+            Console.WriteLine("Mouse Enter");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bmp;
+            }
+
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                //we need to create a Graphics object to draw on the picture box, its our main tool
+                //when making a Pen object, you can just give it color only or give it color and pen size
+
+                g.DrawLine(new Pen(Color.Black, 1), lastPoint, e.Location);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                //this is to give the drawing a more smoother, less sharper look
+            }
+
+            pictureBox1.Invalidate();       //refreshes the picturebox
+            lastPoint = e.Location;         //keep assigning the lastPoint to the current mouse position
+        }
+    }
+}
